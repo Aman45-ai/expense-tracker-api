8a4acdd3c37776b7b1ae243e071ece657883cdc1
Aman45-ai <aman.45official@outlook.com>
2026-09-21 10:55:09 +0530

    fix: remove duplicate backend files

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./src/app.js"
-
-let port = 3000
-
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import authControllers from '../controllers/auth.controller.js'
-
-const router = Router()
-
-router.post('/sign-up',authControllers.signup)
-router.post('/login',authControllers.login)
-
-export default router
